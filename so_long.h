5a764eccfdc04b0714d62a8feade15d3392e697d
Alric Le Conte <ale-cont@student.42lyon.fr>
2023-01-09 10:01:30 +0100

    update lib

--- so_long.h
+++ so_long.h
@@ -6,25 +6,17 @@
 /*   By: ale-cont <ale-cont@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/20 18:37:11 by ale-cont          #+#    #+#             */
-/*   Updated: 2023/01/04 17:23:08 by ale-cont         ###   ########.fr       */
+/*   Updated: 2023/01/09 09:59:43 by ale-cont         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SO_LONG_H
 # define SO_LONG_H
 
-# include <unistd.h>
-# include <stdlib.h>
-# include <stdio.h>
 # include <fcntl.h>
 # include <errno.h>
 # include <string.h>
-
-# ifdef __APPLE__
-#  include "mlx/mlx.h"
-# else
-#  include "mlx_linux/mlx.h"
-# endif
+# include "mlx/mlx.h"
 # include "libft/libft.h"
 
 # define X_EVENT_KEY_PRESS		2
